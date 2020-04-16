34024cefdb2d59f18c7eccafe1c99655a056f0c1
arifannyrs <arifannyrs@gmail.com>
2020-04-16 16:49:48 +0700

    install firebase dan firebaseconfig

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyAQX4Bgu_-7qwsNWuKXcSiisCuhu5-DVUc",
+    authDomain: "fir-react-b2e94.firebaseapp.com",
+    databaseURL: "https://fir-react-b2e94.firebaseio.com",
+    projectId: "fir-react-b2e94",
+    storageBucket: "fir-react-b2e94.appspot.com",
+    messagingSenderId: "479170393890",
+    appId: "1:479170393890:web:28d1f76dbc76ca27a9977b",
+    measurementId: "G-X5B8QNLJFC"
+}
